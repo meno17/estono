6a852fb888fbacc87a80c61364eaaa781ff570dd
meno17 <98544440+meno17@users.noreply.github.com>
2022-02-27 23:29:19 -0600

    Add files via upload

---
+++ src/login_202000886/JOptioPane.java
@@ -0,0 +1,18 @@
+package login_202000886;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author josé
+ */
+class JOptioPane {
+
+    static void showMessageDialog(Object object, String los_datos_no_son_correctos) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
